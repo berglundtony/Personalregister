2fa435a273b235016d0a27008c4dceb57df2dc96
Tony Berglund <tony_berglund@outlook.com>
2021-06-15 18:38:00 +0200

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Personalregister
+{
+    class Employee
+    {
+        public string Firstname;
+        public string Lastname;
+        public int Salary;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Personalregister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            List<Employee> _employees = new List<Employee>();
+         
+            int number;
+            bool success;
+            string quit;
+
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("Förnamn: ");
+                employee.Firstname = Console.ReadLine();
+                Console.WriteLine("Efternamn: ");
+                employee.Lastname = Console.ReadLine();
+                Console.WriteLine("Lön: ");
+                success = int.TryParse(Console.ReadLine(), out number);
+                if (success)
+                {
+                    employee.Salary = number;
+                }
+                else
+                {
+                    Console.WriteLine("Lön måste anges i heltal ");
+                }
+
+                //Employees employees = new Employees();
+                //employees._employees = employees.AddEmployee(employee);
+                _employees.Add(new Employee { Firstname = employee.Firstname, Lastname = employee.Lastname, Salary = employee.Salary });
+
+                Console.Write("********************************************************");
+
+
+                foreach (var item in _employees)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(item.Firstname + " " + item.Lastname + " " + item.Salary);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Vill du mata in fler tryck valfri tangent annars tryck q");
+                ConsoleKeyInfo info = Console.ReadKey();
+                quit = info.KeyChar.ToString();
+                if (quit == "q")
+                {
+                    break;    
+                };
+
+            } while (quit != "q");
+            Environment.Exit(0);
+        }
+
+    }
+}
+
